638396217fa3c81ed36591844302fb12a4940be1
bhargavi-123 <44641739+bhargavi-123@users.noreply.github.com>
2018-11-12 15:43:21 +0530

    Add files via upload

---
+++ PaymentWallet/src/com/capg/paymentwallet/dao/IAccountDao.java
@@ -0,0 +1,13 @@
+package com.capg.paymentwallet.dao;
+
+
+import com.capg.paymentwallet.bean.AccountBean;
+
+public interface IAccountDao {
+
+	public boolean createAccount(AccountBean accountBean) throws Exception ;
+    public boolean updateAccount(AccountBean accountBean) throws Exception;
+    public AccountBean findAccount(int accountId) throws Exception;
+  
+  
+}
